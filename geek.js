d47f217f28bce8e140ab24e591508d7fa2129f47
Malcolm Young <malcolm.young@capgemini.com>
2015-08-12 23:12:30 +0100

    get scores and playlists from external file

--- geek.js
+++ geek.js
@@ -5,21 +5,31 @@ angular.module('geek', ['spotify'])
   SpotifyProvider.setScope('playlist-read-private');
 
 })
-  .controller('geekController', ['$scope', '$sce', 'Spotify', function ($scope, $sce, Spotify) {
+  .controller('geekController', ['$scope', '$http', '$sce', 'Spotify', function ($scope, $http, $sce, Spotify) {
 
     $scope.authenticated = false;
 
-    $scope.playlists = [
-      {user: 'overload74', id: '1Skbt2wll5trx5yHKzjrMt', name: 'May 2015'},
-      {user: 'overload74', id: '3uHRcW7iw4Q2rwvZv3Ovv4', name: 'June 2015'},
-      {user: 'overload74', id: '6M8VU8JnWD05Q9n9qZkrn9', name: 'July 2015'}
-    ]
-
+    $scope.playlists = []
     $scope.playlistsLookup = {};
 
-    for (var i = 0, len = $scope.playlists.length; i < len; i++) {
-      $scope.playlistsLookup[$scope.playlists[i].id] = $scope.playlists[i];
-    }
+    $http.get('data/playlists.json')
+      .then(function(res){
+        $scope.playlists = res.data.playlists;
+        for (var i = 0, len = $scope.playlists.length; i < len; i++) {
+          $scope.playlistsLookup[$scope.playlists[i].id] = $scope.playlists[i];
+        }
+      });
+
+
+    $scope.scores = []
+    $scope.scoresLookup = {};
+    $http.get('data/scores.json')
+      .then(function(res){
+        $scope.scores = res.data.scores;
+          for (var i = 0, len = $scope.scores.length; i < len; i++) {
+          $scope.scoresLookup[$scope.scores[i].id] = $scope.scores[i];
+        }
+      });
 
     $scope.updatePlaylist = function () {
       $scope.loadedPlaylist = $scope.playlistsLookup[$scope.selectedPlaylist];
@@ -32,6 +42,14 @@ angular.module('geek', ['spotify'])
 
         for (var index in $scope.tracks) {
           $scope.tracks[index].hits = $scope.tracks[index].misses = $scope.tracks[index].maybes = 0;
+
+          var track = $scope.tracks[index].track;
+
+          if (track.id in $scope.scoresLookup) {
+            $scope.tracks[index].hits = $scope.scoresLookup[track.id].hits;
+            $scope.tracks[index].misses = $scope.scoresLookup[track.id].misses;
+            $scope.tracks[index].maybes = $scope.scoresLookup[track.id].maybes;
+          }
         }
 
         var embedUrl = "https://embed.spotify.com/?uri=spotify%3Auser%3A" + data.owner.id + "%3Aplaylist%3A" + data.id;
